d04c0620b8d1143725d46e176e22d947b390c235
rabbitali <wenxin32@foxmail.com>
2024-01-31 13:04:45 +0800

    add method to parse yaml file

--- vulcanus/conf/__init__.py
+++ vulcanus/conf/__init__.py
@@ -17,6 +17,8 @@ Description: global config.
 """
 import os
 import configparser
+import inspect
+import yaml
 
 from vulcanus.conf import default_config
 from vulcanus.conf.constant import SYSTEM_CONFIG_PATH, PRIVATE_INDIVIDUATION_CONFIG
@@ -103,6 +105,102 @@ class Config:
             self._set_section_value(conf, section)
 
         self._load_private_config()
+
+
+class JsonObject:
+    def __init__(self, data):
+        """
+        Initialize the JsonObject object.
+
+        Args:
+            data (dict): Data to be converted to a JsonObject.
+        """
+        for key, value in data.items():
+            if isinstance(value, dict):
+                setattr(self, key, JsonObject(value))
+            elif isinstance(value, list):
+                setattr(self, key, [JsonObject(v) if isinstance(v, dict) else v for v in value])
+            else:
+                setattr(self, key, value)
+
+    def __getattr__(self, attr):
+        return self.__dict__.get(attr)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class YamlConfigParse:
+    def __init__(self, config_file, default=None):
+        """
+        Initialize the Config object.
+
+        Args:
+            config_file (str): Path to the YAML configuration file.
+            default (module): The default configuration module (optional).
+        """
+        data = self.get_default_config_to_dict(default)
+        data.update(self.parse_yaml(config_file))
+        self.parser = JsonObject(data)
+
+    @staticmethod
+    def parse_yaml(config_file):
+        """
+        Parse the YAML configuration file and return the parsed data.
+
+        Args:
+            config_file (str): Path to the YAML configuration file.
+
+        Returns:
+            Any: Parsed data from the YAML configuration file.
+
+        Raises:
+            RuntimeError: If the configuration file does not exist or fails to load.
+        """
+        if not os.path.exists(config_file):
+            raise RuntimeError(f"Configuration file does not exist: {config_file}")
+        try:
+            with open(config_file, 'r') as file:
+                return yaml.safe_load(file)
+
+        except IOError:
+            raise RuntimeError(f"Failed to load the configuration file: {config_file}")
+
+        except yaml.YAMLError:
+            raise RuntimeError(f"Failed to parse the configuration file: {config_file}")
+
+    @staticmethod
+    def get_default_config_to_dict(default_config_module):
+        """
+        Convert variables defined in a default configuration module to a dictionary.
+
+        Args:
+            default_config_module (module): The default configuration module.
+
+        Returns:
+            dict: Dictionary containing the variables and their values defined in the default configuration module.
+        """
+        default_config_dict = {}
+
+        if default_config_module is None:
+            return default_config_dict
+
+        for name, value in inspect.getmembers(default_config_module):
+            if not name.startswith("__") and not inspect.ismodule(value):
+                default_config_dict[name] = value
+        return default_config_dict
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the parsed configuration.
+
+        Returns:
+            str: String representation of the parsed configuration.
+        """
+        return str(self.parser)
 
 
 configuration = Config(SYSTEM_CONFIG_PATH, default_config)
